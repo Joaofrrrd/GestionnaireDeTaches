3c6e7eb3a3286f4b48b78ff194312340aceeaa16
Joaofrrrd <joao.frrrd@eduge.ch>
2023-11-24 11:22:17 +0100

    version finale

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.gestionnairedetaches"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.gestionnairedetaches"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/gestionnairedetaches/MainActivity.kt
+++ app/src/main/java/com/example/gestionnairedetaches/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gestionnairedetaches.ui.theme.GestionnaireDeTachesTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,18 +39,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(titre : String, texte : String, modifier: Modifier = Modifier) {
-    Column {
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null)
-        Text(
-            text = titre
+fun Greeting(titre: String, texte: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null,
+            modifier = Modifier.padding(8.dp)
         )
         Text(
-            text = texte
+            text = titre,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 24.dp, end = 8.dp)
         )
-            
-            
+        Text(
+            text = texte,
+            fontSize = 16.sp
+        )
     }
 }
 
